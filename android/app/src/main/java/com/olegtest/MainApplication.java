1d915f9098c5a718a1bdb42298ac700582d93e01
getlittletech <getlittletech@gmail.com>
2017-09-28 13:38:55 +0200

    Add mqtt client to the native projects and add a quick js code to make sure it works.

--- android/app/src/main/java/com/olegtest/MainApplication.java
+++ android/app/src/main/java/com/olegtest/MainApplication.java
@@ -7,9 +7,12 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.tuanpm.RCTMqtt.RCTMqttPackage;
 
 import java.util.Arrays;
 import java.util.List;
+
+// Add this to the imports in the top of your file:
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RCTMqttPackage()
       );
     }
   };
--- src/main/App.js
+++ src/main/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import mqtt from '../../lib/react-native-mqtt'
+
 import {
   StyleSheet,
   Text,
@@ -38,4 +40,34 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+});
+
+mqtt.createClient({
+  uri: 'mqtt://test.mosquitto.org:1883',
+  clientId: 'your_client_id'
+}).then(function(client) {
+
+  client.on('closed', function() {
+    console.log('mqtt.event.closed');
+
+  });
+
+  client.on('error', function(msg) {
+    console.log('mqtt.event.error', msg);
+
+  });
+
+  client.on('message', function(msg) {
+    console.log('mqtt.event.message', msg);
+  });
+
+  client.on('connect', function() {
+    console.log('connected');
+    client.subscribe('/data', 0);
+    client.publish('/data', "test", 0, false);
+  });
+
+  client.connect();
+}).catch(function(err){
+  console.log(err);
 });
